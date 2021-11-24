c9ff636d16b3054f4a9f831ba41886b4bd1d274c
bolgera129 <bolgera@bc.edu>
2021-11-24 02:50:59 -0500

    Updates

---
+++ precache-manifest.63d0e834f5f4e9e23f3bf0004e645cbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd307e8a8534e2b5fc59c4fa63943af",
+    "url": "/bored-website/index.html"
+  },
+  {
+    "revision": "2bf45de5b6c402228945",
+    "url": "/bored-website/static/css/2.d7af4fa5.chunk.css"
+  },
+  {
+    "revision": "e247f58da243f02fb4cf",
+    "url": "/bored-website/static/css/main.b3747119.chunk.css"
+  },
+  {
+    "revision": "2bf45de5b6c402228945",
+    "url": "/bored-website/static/js/2.876b5e7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bored-website/static/js/2.876b5e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e247f58da243f02fb4cf",
+    "url": "/bored-website/static/js/main.4936b668.chunk.js"
+  },
+  {
+    "revision": "0d7fc71ba235223a1c11",
+    "url": "/bored-website/static/js/runtime-main.a078538b.js"
+  },
+  {
+    "revision": "95aed62101ca46f8d8196271f7d72319",
+    "url": "/bored-website/static/media/Billy_Ohio.95aed621.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bored-website/precache-manifest.63d0e834f5f4e9e23f3bf0004e645cbb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bored-website/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
